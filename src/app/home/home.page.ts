36b2f944307e95e3581a50c01072e8a396f7ac0b
jjunieko <juniorgtr123@gmail.com>
2021-02-04 18:14:09 -0300

    loading

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+
   }
+
 
 }
--- src/app/pages/preload-chat/preload-chat.page.ts
+++ src/app/pages/preload-chat/preload-chat.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-preload-chat',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./preload-chat.page.scss'],
 })
 export class PreloadChatPage implements OnInit {
-
-  constructor() { }
+  public carregando: any;
+  constructor(public loading: LoadingController) { }
 
   ngOnInit() {
+    this.presentLoading();
   }
+  async presentLoading() {
+    const loading = await this.loading.create({
+      cssClass: 'my-custom-class',
+      message: ' Ao clicar no botão "entrar" você será direcionado para o chat do Coletivo Motirõ.',
+      duration: 5000
+    });
+    await loading.present();
 
+  }
+  
 }
