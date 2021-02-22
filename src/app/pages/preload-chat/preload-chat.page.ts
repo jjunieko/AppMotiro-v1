163d54775f8bc493e14ad227ae3377393d86e945
jjunieko <juniorgtr123@gmail.com>
2021-02-22 16:25:07 -0300

    versão v1 from final

--- src/app/pages/preload-chat/preload-chat.page.ts
+++ src/app/pages/preload-chat/preload-chat.page.ts
@@ -18,7 +18,7 @@ export class PreloadChatPage implements OnInit {
     const loading = await this.loading.create({
       cssClass: 'my-custom-class',
       message: ' Ao clicar no botão "entrar" você será direcionado para o chat do Coletivo Motirõ.',
-      duration: 5000
+      duration: 3000
     });
     await loading.present();
 
